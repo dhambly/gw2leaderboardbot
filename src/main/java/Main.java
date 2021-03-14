42fabb67fff6d7419e66393fa4404ca48fbe7acd
dhambly <derickhambly@gmail.com>
2021-03-14 00:24:19 -0600

    Update config file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,13 @@ import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws LoginException, IOException {
         Properties prop = new Properties();
-        prop.load(new FileInputStream("src/main/config.properties"));
+        prop.load(new FileInputStream("config.properties"));
         String token = prop.getProperty("token");
         JDABuilder builder = JDABuilder.createDefault(token);
         APIHandler api = new APIHandler();
